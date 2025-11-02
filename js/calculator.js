32c2a73ea19027f85e658c6300a241a38baad95e
Rahmanberdi <rahmanberdihojanazarow@gmail.com>
2025-11-02 23:13:17 +0800

    Added history and improved calcultaion

--- js/calculator.js
+++ js/calculator.js
@@ -1,66 +1,124 @@
 
-    let input_1 ="";
-    let input_2 ="";
-    let old_ops="";
-    const numbers = document.querySelectorAll('button.number');
-    numbers.forEach(number => {
-        number.addEventListener("click", () => {
-            input_1 = input_1+number.textContent;
-            document.querySelector('div.display-main').innerText = input_1;
-        })
+    let input_1 = "";
+    let input_2 = "";
+    let history = "";
+    let ops="";
+    let last_num = 0;
+    let math_ops = ['+','-','×','÷','%'];
+    let calc_ops = ['C','backspace','=','+/-'];
+
+    function updateDisplay(value,display){
+        if (display === 1){
+            document.querySelector('div.display-main').innerText = value;
+        }else{
+            document.querySelector('div.display-secondary').innerText = value;
+        }
+    }
+    function handleInputKey(key) {
+            if (!isNaN(key) || (key === "." && !input_1.includes('.'))) {
+                input_1 += key;
+                updateDisplay(input_1,1);
+            }else if (math_ops.includes(key)) {
+                if (input_2==="" && input_1!==""){
+                    input_2 = input_1;
+                    history += Number(input_2)+key;
+                    updateDisplay(history,2);
+                    input_1 = "";
+                    ops = key;
+                }else if (input_2!=="" && input_1!==""){
+                    input_2 = calculate(input_2,input_1,ops);
+                    if (isNaN(history[history.length-1])){
+                        history+=Number(input_1);
+                    }else{
+                        history+=ops+Number(input_1);
+                    }
+                    updateDisplay(input_2,1);
+                    updateDisplay(history,2);
+                    history = ""+Number(input_1);
+                    ops = key;
+                    input_1 = "";
+                }else if (input_2!=="" && input_1===""){
+                    ops = key;
+                }
+            }else if (calc_ops.includes(key)){
+                if (input_1!=="" && input_2!=="" && key==='='){
+                    result = calculate(input_2,input_1,ops);
+                    last_num = input_1;
+                    input_2 = result;
+
+                    if (isNaN(history[history.length-1])){
+                        history+=Number(input_1);
+                    }else{
+                        history+=ops+Number(input_1);
+                    }
+                    updateDisplay(result,1);
+                    updateDisplay(history,2);
+                    input_1 = "";
+                    history = ""+Number(input_2);
+
+                }else if (input_1==="" && input_2!=="" && key==='='){
+                    result = calculate(input_2,last_num,ops);
+                    input_2 = result;
+                    history = '';
+                    updateDisplay(result,1);
+                    updateDisplay(history,2);
+                    history = ""+Number(input_2);
+                }
+                else{
+                    if (key==='backspace'){
+                        input_1 = input_1.substring(0,input_1.length-1);
+                    }else if(key==='C'){
+                        input_1 = '';
+                        input_2 = '';
+                        ops = '';
+                        history = '';
+                        updateDisplay(history,2);
+                    }else if(key==='+/-'){
+                        input_1 = input_1*(-1);
+                    }
+                    updateDisplay(input_1,1);
+                }
+
+            } else updateDisplay(input_1,1);
+    }
+
+
+    document.querySelectorAll('button').forEach(btn => {
+        btn.addEventListener('click', () => {
+            let value = btn.textContent.trim();
+
+            // Map your display symbols to actual operators
+
+            if (btn.id === "backspace") value = "backspace";
+
+            handleInputKey(value);
+        });
+    });
+    document.addEventListener("keydown", (e) => {
+        let key = e.key;
+
+        if (key === "Enter") key = '=';
+        else if (key === "Backspace") key = 'backspace';
+        else if (key === "Delete") key = 'C';
+        if (math_ops.includes(value) || calc_ops.includes(value) || value === "."){
+            handleInputKey(key);
+        }
+
     })
-    const operators  = document.querySelectorAll('button.operator');
-    operators.forEach(operator => {
-        operator.addEventListener("click", () => {
-            if (operator.textContent==='C'){
-                input_1='';
-                input_2='';
-                document.querySelector('div.display-main').innerText = input_1;
-                old_ops ='';
-            }else if(operator.id==='clear'){
-                input_1=input_1.substring(0,input_1.length-1);
-                document.querySelector('div.display-main').innerText = input_1;
+function calculate(a,b,action){
+            let result;
+            a=Number(a);
+            b=Number(b);
+            if (action ==="+") {
+                result = a+b;
+            }else if(action==="-") {
+                result = a-b;
+            }else if(action==="×") {
+                result = a*b;
+            }else if(action==="÷") {
+                result = a/b;
+            }else if (action==='%'){
+                result = a*b/100;
             }
-            else if (input_2 !== "" && operator.textContent!=='+/-') {
-                calculator(Number(input_1),Number(input_2),old_ops,operator.textContent);
-            }
-            else if(operator.textContent==='+/-'){
-                input_1=input_1*(-1);
-                document.querySelector('div.display-main').innerText = input_1;
-            }
-            else if(operator.textContent!=='='){
-                input_2 = input_1;
-                old_ops = operator.textContent;
-                input_1 = "";
-            }
-
-
-        })
-    })
-
-function calculator(a,b,ops,action){
-
-
-            if (ops ==="+") {
-                input_2 = b+a;
-            }else if(ops==="-") {
-                input_2 = b-a;
-            }else if(ops==="×") {
-                input_2 = a*b;
-            }else if(ops==="÷") {
-                input_2 = b/a;
-            }else if (ops==='%'){
-                input_2 = b*a/100;
-            }
-            if (action==='=' && old_ops !== "") {
-                document.querySelector('div.display-main').innerText = input_2;
-                input_1 = input_2;
-                input_2 = "";
-                old_ops="";
-            }else{
-                document.querySelector('div.display-main').innerText = input_2;
-                input_1 = "";
-                old_ops=action;
-            }
-
+            return result;
 }
